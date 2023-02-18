ac7f09c05d770ffdf96e3704c0a4a93773500b1b
swatibersurda <swatibersurda@gmail.com>
2023-02-18 10:37:19 +0530

    adding_step_page

--- addingtwonumbers/src/Pages/Home.jsx
+++ addingtwonumbers/src/Pages/Home.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { postTwoNumbers } from "../Redux/action";
 import { StepPage } from "./StepPage";
+import * as style from  "../Style/styling.css";
 
 export const Home = () => {
   const dispatch = useDispatch();
@@ -22,23 +23,42 @@ export const Home = () => {
   };
 
   return (
-    <div>
-      i am home
-      <input
-        type={"string"}
-        value={a}
-        onChange={(e) => {
-          setA(e.target.value);
-        }}
-      />
-      <input
-        type={"string"}
-        value={b}
-        onChange={(e) => {
-          setB(e.target.value);
-        }}
-      />
+    <div className="container">
+      <div className="topMostDiv">
+        <div className="ineerStepTextDiv">
+           <h1>Step Addition</h1> 
+        </div>
+      </div>
+      <div>
+
+      </div>
+      <div className="inputTakingDiv">
+        <div className="labelDivOne">
+            First Number:
+        </div>
+        <input
+          type={"string"}
+          value={a}
+          onChange={(e) => {
+            setA(e.target.value);
+          }}
+        />
+      </div>
+      <div className="inputTakingDiv">
+      <div className="labelDivTwo">
+        Second Number:
+      </div>
+        <input
+          type={"string"}
+          value={b}
+          onChange={(e) => {
+            setB(e.target.value);
+          }}
+        />
+      </div>
+      <div className="genrateButtonDiv" onClick={handleAdd}>
       <button onClick={handleAdd}>Genrate Steps</button>
+      </div>
       {/* conditional rendering so that if there is no data it should not show any error and dont display the steps */}
       {carrData ? <StepPage /> : ""}
     </div>
--- addingtwonumbers/src/Pages/StepPage.jsx
+++ addingtwonumbers/src/Pages/StepPage.jsx
@@ -1,16 +1,25 @@
 import { useDispatch, useSelector } from "react-redux";
+import * as style from  "../Style/styling.css";
 export const StepPage=()=>{
     // extracting the data we got from api carryArray and addData.
   const carrData = useSelector((state) => state.data.carryArray);
   const addData = useSelector((state) => state.data.addArray);
-  console.log(carrData, "reached",addData,"dd");
+//   console.log(carrData, "reached",addData,"dd");
     return(
-        <div>
-            {carrData&&carrData.map((item,index)=>{
-              return  <div>
-                  <h1>step{index}{carrData}{addData}</h1>
+        <div className="stepContainer">
+            <div className="steps">
+            {carrData&&addData.map((item,index)=>{
+              return  <div key={index} className="innerStepsDiv" >
+                  {/* <p>step{index+1}</p>
+                   */}
+                   <div className="contentDiv">
+                   <h3>step{index+1}:</h3>
+                   <h3>{`{`}"carryString":{carrData[index]}{`,`}"sumString":{addData[index]}{`}`}</h3>
+                   </div>
               </div>
             })}
+            </div>
+          
           
          
         </div>
